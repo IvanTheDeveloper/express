a6aa5ac1042d56f62df67eae785e68d63c12ec5f
git (Ivan) <email>
2024-06-10 01:52:18 +0200

    desplegadi

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const sendEmail = require("./mailer");
 
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.json("Hello world!");
 });
 
-app.get("/mail", async (req, res) => {
+app.get("/send", async (req, res) => {
   try {
     const { email, subject, message } = req.query;
 
     const info = await sendEmail(email, subject, message);
-    res.send("Correo enviado exitosamente: " + info);
+    res.json("Correo enviado exitosamente: " + info);
   } catch (error) {
     console.error("Error al enviar el correo: ", error);
-    res.status(500).send("Error al enviar el correo: " + error);
+    res.status(500).json("Error al enviar el correo: " + error);
   }
 });
 
